cbfad6366d8c15ae083a1741ce82efef7b3257ee
Alexander Pyatkin <asp@thexyz.net>
2015-07-12 15:54:41 +0400

    Environment variables for loggers

--- site-cookbooks/themis/attributes/default.rb
+++ site-cookbooks/themis/attributes/default.rb
@@ -1,3 +1,5 @@
 default[:themis][:basedir] = '/var/themis/finals'
 default[:themis][:repository] = 'https://github.com/aspyatkin/themis-finals'
 default[:themis][:revision] = 'master'
+default[:themis][:log_level] = 'INFO'
+default[:themis][:stdout_sync] = 'false'
