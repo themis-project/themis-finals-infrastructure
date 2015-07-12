5f982990557b2f912fbe9c3788261ab0997dd092
Alexander Pyatkin <asp@thexyz.net>
2015-07-12 20:38:44 +0400

    Add checker libraries cookbooks

---
+++ site-cookbooks/checker-py/attributes/default.rb
@@ -0,0 +1,5 @@
+node_id = 'checker-py'
+
+default[node_id][:basedir] = '/var/themis/checker-py'
+default[node_id][:repository] = 'https://github.com/aspyatkin/themis-checker-py'
+default[node_id][:revision] = 'master'
---
+++ site-cookbooks/checker-rb/attributes/default.rb
@@ -0,0 +1,5 @@
+node_id = 'checker-rb'
+
+default[node_id][:basedir] = '/var/themis/checker-rb'
+default[node_id][:repository] = 'https://github.com/aspyatkin/themis-checker-rb'
+default[node_id][:revision] = 'master'
