0be743c656b2502ce331549bf8a851be4fbe7514
Alexander Pyatkin <asp@thexyz.net>
2015-08-04 21:20:13 +0400

    Add attack-result cookbook

---
+++ site-cookbooks/attack-result-rb/attributes/default.rb
@@ -0,0 +1,5 @@
+node_id = 'attack-result-rb'
+
+default[node_id][:basedir] = '/var/themis/attack-result-rb'
+default[node_id][:repository] = 'https://github.com/aspyatkin/themis-attack-result'
+default[node_id][:revision] = 'master'
