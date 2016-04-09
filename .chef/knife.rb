debd2723a928b964a9c3461721457b0099cc0114
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-04-09 19:41:03 +0400

    Update knife configuration

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,8 +1,13 @@
-cookbook_path    ["cookbooks", "site-cookbooks"]
-node_path        "nodes"
-role_path        "roles"
-environment_path "environments"
-data_bag_path    "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
+require 'dotenv'
+Dotenv.load
 
-knife[:berkshelf_path] = "cookbooks"
+cookbook_path ['cookbooks', 'site-cookbooks']
+node_path 'nodes'
+role_path 'roles'
+environment_path 'environments'
+data_bag_path 'data_bags'
+
+environment = ENV['CHEF_ENV'] || 'development'
+encrypted_data_bag_secret "encryption_keys/#{environment}_key"
+
+knife[:berkshelf_path] = 'cookbooks'
